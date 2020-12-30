ebc9330b1fcbc8963c736817c1db66d97d61da13
a719743297 <73977191+a719743297@users.noreply.github.com>
2020-12-30 16:19:19 +0800

    Update and rename src/lib/config.js to SRC/利布/config.js

---
+++ SRC/利布/config.js
@@ -0,0 +1,10 @@
+config={
+  //cookie里边的sid
+  sid:'V02SitZNdI1NGzBkOEypvx37NdQxr9000a4ca9bf001f711c05',
+ //邀请id 也就是账号id
+  inviteid:'527506437',
+  //server酱key 自行百度
+  sckey:'SCU141843T2346f6f1e05e9b00e26095b3bdda63845fec36d67a449'
+}
+
+module.exports=config
